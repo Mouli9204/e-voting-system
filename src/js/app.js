b28932aa459d9b170b265bb9f18f2f2cc4adfe44
PandiarajS1 <s.pandiarajsekar@gmail.com>
2025-03-10 00:52:06 +0530

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,36 @@
 App = {
   web3Provider: null,
   contracts: {},
-  account: '0x0',
+  account: "0x0",
   hasVoted: false,
 
   init: function () {
     return App.initWeb3();
   },
 
-  initWeb3: function () {
+  initWeb3: async function () {
     // TODO: refactor conditional
-    if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    } else if (window.web3) {
+      // Legacy dApp browsers
+      App.web3Provider = window.web3.currentProvider;
+      web3 = new Web3(window.web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-      ethereum.enable();
+      // Fallback for no web3 instance
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:8545"
+      );
       web3 = new Web3(App.web3Provider);
     }
+
     return App.initContract();
   },
 
@@ -43,21 +54,29 @@ App = {
       // Restart Chrome if you are unable to receive this event
       // This is a known issue with Metamask
       // https://github.com/MetaMask/metamask-extension/issues/2393
-      instance.votedEvent({}, {
-        fromBlock: 'latest',
-        toBlock: 'latest'
-      }).watch(function (error, event) {
-        console.log("event triggered", event)
-        // Reload when a new vote is recorded
-        App.render();
-      });
+
+      instance
+        .votedEvent(
+          {},
+          {
+            fromBlock: "latest",
+            toBlock: "latest",
+          }
+        )
+        .watch(function (error, event) {
+          console.error("event Watch Error", error);
+          console.log("event triggered", event);
+          // Reload when a new vote is recorded
+          console.log(App.render());
+          App.render();
+        });
     });
   },
   listenForAccountChange: function () {
-    ethereum.on('accountsChanged', function (accounts) {
+    ethereum.on("accountsChanged", function (accounts) {
       App.account = accounts[0];
       App.render();
-    })
+    });
   },
 
   render: function () {
@@ -71,11 +90,13 @@ App = {
     // Load account data
 
     web3.eth.getAccounts(function (err, account) {
-      $("#accountAddress").html(`<span style="font-size:14px;">Your Voter's Address: 
+      $(
+        "#accountAddress"
+      ).html(`<span style="font-size:14px;">Your Voter's Address: 
       <span style="font-weight:600">${account[0]}</span></span><br><br>
       <small>(Refresh the page if you've migrated to new account)</small>`);
       App.account = account[0];
-      $('#bv-address').text(`${App.account}`)
+      $("#bv-address").text(`${App.account}`);
       document.getElementById("bv-qr-code").innerHTML = "";
 
       new QRCode(document.getElementById("bv-qr-code"), {
@@ -84,73 +105,78 @@ App = {
         height: 170,
         colorDark: "#000000",
         colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H
+        correctLevel: QRCode.CorrectLevel.H,
       });
     });
 
     // Load contract data
-    App.contracts.Election.deployed().then(function (instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function (candidatesCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
+    App.contracts.Election.deployed()
+      .then(function (instance) {
+        electionInstance = instance;
+        return electionInstance.candidatesCount();
+      })
+      .then(function (candidatesCount) {
+        var candidatesResults = $("#candidatesResults");
+        candidatesResults.empty();
 
-      var candidatesSelect = $('#candidatesSelect');
-      candidatesSelect.empty();
+        var candidatesSelect = $("#candidatesSelect");
+        candidatesSelect.empty();
 
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function (candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[3];
-          var party = candidate[2];
-          // Render candidate Result
-          var candidateTemplate =
-            `<tr><th>${id}</th><td>${name}</td><td>${party}</td><td>${voteCount}</td></tr>`;
-          candidatesResults.append(candidateTemplate);
+        for (var i = 1; i <= candidatesCount; i++) {
+          electionInstance.candidates(i).then(function (candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[3];
+            var party = candidate[2];
+            // Render candidate Result
+            var candidateTemplate = `<tr><th>${id}</th><td>${name}</td><td>${party}</td><td>${voteCount}</td></tr>`;
+            candidatesResults.append(candidateTemplate);
 
-          // Render candidate ballot option
-          var candidateOption = `<option value="${id}"> ${name} (${party}) </option>`
-          candidatesSelect.append(candidateOption);
-        });
-      }
-      return electionInstance.voters(App.account);
-    }).then(function (hasVoted) {
-      // Do not allow a user to vote
-      if (hasVoted) {
-        $('form').hide();
-        $('#vote-msg').html(`<div class="col-sm-6 offset-sm-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3">
+            // Render candidate ballot option
+            var candidateOption = `<option value="${id}"> ${name} (${party}) </option>`;
+            candidatesSelect.append(candidateOption);
+          });
+        }
+        return electionInstance.voters(App.account);
+      })
+      .then(function (hasVoted) {
+        // Do not allow a user to vote
+        if (hasVoted) {
+          $("form").hide();
+          $("#vote-msg")
+            .html(`<div class="col-sm-6 offset-sm-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3">
         <div class="alert alert-danger text-center" role="alert">
           <span>You have already voted!</span>
         </div>
-      </div>`)
-        $('#bv-voted').text(`Yes`)
-      }
-      else {
-        $('#bv-voted').text(`No`)
-
-      }
-      loader.hide();
-      content.show();
-    }).catch(function (error) {
-      //console.warn(error);
-    });
+      </div>`);
+          $("#bv-voted").text(`Yes`);
+        } else {
+          $("#bv-voted").text(`No`);
+        }
+        loader.hide();
+        content.show();
+      })
+      .catch(function (error) {
+        //console.warn(error);
+      });
   },
 
   castVote: function () {
-    var candidateId = $('#candidatesSelect').val();
-    App.contracts.Election.deployed().then(function (instance) {
-      return instance.vote(candidateId, { from: App.account });
-    }).then(function (result) {
-      // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
-      alert("Thanks for voting")
-    }).catch(function (err) {
-      console.error(err);
-    });
-  }
+    var candidateId = $("#candidatesSelect").val();
+    App.contracts.Election.deployed()
+      .then(function (instance) {
+        return instance.vote(candidateId, { from: App.account });
+      })
+      .then(function (result) {
+        // Wait for votes to update
+        $("#content").hide();
+        $("#loader").show();
+        alert("Thanks for voting");
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  },
 };
 
 $(function () {
